458c61868ec2b52c71328ad75a01422340cfbaf5
Farleygood <44248556@qq.com>
2016-10-28 10:49:42 +0800

    添加注释信息

--- WriteEcecl.py
+++ WriteEcecl.py
@@ -2,17 +2,19 @@
 # -*- coding:utf-8 -*-
 __author__ = 'Administrator'
 
+# 创建一个工作薄
 f = xlwt.Workbook()
+# 创建一个工作表，命名为‘预约号’
 sheet1 = f.add_sheet(u'预约号', cell_overwrite_ok=True)
 l = []
 for i in range(100):
     j = random.randint(12340000,12349999)
-    d = 'CM'+str(j)
+    d = 'CM'+str(j)    #将数字转换成字符串，用CM拼接
     l.append(d)
 # print '\n'.join(l)
 
 for n in range(100):
-    sheet1.write(0,0,u'这是预约号')
-    sheet1.write(n+1,0,l[n])
-f.save('test.xls')
+    sheet1.write(0,0,u'这是预约号')      #在第一行写入标题
+    sheet1.write(n+1,0,l[n])         #从第二行开始，每行写人一个预约号码
+f.save('test.xls')                   #一定要记得保存
      
