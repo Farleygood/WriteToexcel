7df03bc37e274fc017d9f920f83201fb5248fe71
Farleygood <44248556@qq.com>
2016-10-28 11:55:34 +0800

    优化for循环

--- WriteEcecl.py
+++ WriteEcecl.py
@@ -2,6 +2,9 @@
 # -*- coding:utf-8 -*-
 __author__ = 'Administrator'
 
+import random
+import xlwt
+
 # 创建一个工作薄
 f = xlwt.Workbook()
 # 创建一个工作表，命名为‘预约号’
@@ -12,9 +15,8 @@ for i in range(100):
     d = 'CM'+str(j)    #将数字转换成字符串，用CM拼接
     l.append(d)
 # print '\n'.join(l)
-
-for n in range(100):
+# for n in range(100):
     sheet1.write(0,0,u'这是预约号')      #在第一行写入标题
-    sheet1.write(n+1,0,l[n])         #从第二行开始，每行写人一个预约号码
+    sheet1.write(i+1,0,l[i])         #从第二行开始，每行写人一个预约号码
 f.save('test.xls')                   #一定要记得保存
      
